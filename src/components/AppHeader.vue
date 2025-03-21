c3cd65f17df37ccaf061d13ddc806fd30d44b190
Nikita Gritsenko <nik.gretsenko1@gmail.com>
2025-03-21 18:04:11 +0700

    feat: Добавлен PrimeVue
    Добавлены зависимости PrimeVue и PrimeUIX, обновлен основной компонент приложения, удалены неиспользуемые компоненты и стили, улучшена структура CSS.

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="container">
+      <span> Учётные записи </span>
+      <div>+</div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+}
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
