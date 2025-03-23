9ddfd3ec56eb67d0e7b5c4d213272f90d5ed399f
Nikita Gritsenko <nik.gretsenko1@gmail.com>
2025-03-23 17:24:03 +0700

    chore: замена на PrimeFlex стили и удаление ненужных стилей
    - удалены неиспользуемые стили
    - обычные стили заменены на PrimeFlex стили

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,34 +19,11 @@ const toggleDarkMode = () => {
   <header
     class="flex justify-content-between align-items-center p-3 mb-3 border-bottom-1 border-500"
   >
-    <div class="container">
-      <span> Учётные записи </span>
+    <div class="container flex justify-content-between align-items-center gap-3 ml-3">
+      <span class="font-semibold"> Учётные записи </span>
       <app-button label="Добавить" icon="pi pi-plus" @click="addNewAccount" />
     </div>
     <app-button icon="pi pi-moon" @click="toggleDarkMode" v-if="!isDarkMode" />
     <app-button icon="pi pi-sun" @click="toggleDarkMode" v-else />
   </header>
 </template>
-
-<style scoped>
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem 0;
-  border-bottom: 1px solid #e0e0e0;
-  margin-bottom: 1rem;
-}
-
-span {
-  font-weight: 600;
-}
-
-.container {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 1rem;
-  margin-left: 1rem;
-}
-</style>
--- src/components/AppTip.vue
+++ src/components/AppTip.vue
@@ -1,12 +1,6 @@
 <template>
-  <app-message class="app-tip" severity="warn" icon="pi pi-info-circle" closable
+  <app-message class="app-tip m-3" severity="warn" icon="pi pi-info-circle" closable
     >Для указания нескольких меток для одной пары логин/пароль используйте разделитель
     ;</app-message
   >
 </template>
-
-<style scoped>
-.app-tip {
-  margin: 1rem;
-}
-</style>
